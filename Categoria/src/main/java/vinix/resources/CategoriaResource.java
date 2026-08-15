eafeefb96ea7a7487dd0b7e8020e8de89d695b93
vinicius <viniciusdejjeuspessoal@gmail.com>
2026-08-14 23:31:12 -0300

    Categoria: Resource e exceptions

--- Categoria/src/main/java/vinix/resources/CategoriaResource.java
+++ Categoria/src/main/java/vinix/resources/CategoriaResource.java
@@ -1,9 +1,61 @@
 package vinix.resources;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import vinix.dtos.CategoriaRequestDTO;
+import vinix.dtos.CategoriaResponseDTO;
+import vinix.services.CategoriaService;
 
+import java.net.URI;
+import java.util.List;
+
+@RequiredArgsConstructor
 @RestController
-@RequestMapping(value = "/categorias")
+@RequestMapping("/categorias")
 public class CategoriaResource {
+
+  private final CategoriaService service;
+
+  @GetMapping
+  public ResponseEntity<List<CategoriaResponseDTO>> buscarTodos() {
+    List<CategoriaResponseDTO> list = service.buscarTodos();
+    return ResponseEntity.ok(list);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<CategoriaResponseDTO> buscarPorId(@PathVariable Long id) {
+    CategoriaResponseDTO dto = service.buscarPorId(id);
+    return ResponseEntity.ok(dto);
+  }
+
+  @PostMapping
+  public ResponseEntity<CategoriaResponseDTO> salvar(
+      @Valid @RequestBody CategoriaRequestDTO dto) {
+
+    CategoriaResponseDTO responseDTO = service.salvar(dto);
+
+    URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+        .path("/{id}")
+        .buildAndExpand(responseDTO.id())
+        .toUri();
+
+    return ResponseEntity.created(uri).body(responseDTO);
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<CategoriaResponseDTO> atualizar(
+      @PathVariable Long id, @Valid @RequestBody CategoriaRequestDTO dto) {
+
+    CategoriaResponseDTO responseDTO = service.atualizar(id, dto);
+    return ResponseEntity.ok(responseDTO);
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> deletar(@PathVariable Long id) {
+    service.deletar(id);
+    return ResponseEntity.noContent().build();
+  }
 }
--- Categoria/src/main/java/vinix/resources/exceptions/ResourceExceptionHandler.java
+++ Categoria/src/main/java/vinix/resources/exceptions/ResourceExceptionHandler.java
@@ -2,45 +2,6 @@ package vinix.resources.exceptions;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-import vinix.services.exceptions.ResourceNotFoundException;
-
-import java.time.Instant;
-
-@Slf4j
-@RestControllerAdvice
-public class ExceptionHandler {
-
-  @ExceptionHandler(DataIntegrityViolationException.class)
-  public ResponseEntity<StandardError> dataIntegrityViolation(
-      DataIntegrityViolationException e,
-      HttpServletRequest request) {
-
-    HttpStatus status = HttpStatus.CONFLICT; // 409
-
-    StandardError err = new StandardError(
-        Instant.now(),
-        status.value(),
-        "Conflito de Integridade",
-        "Não é possível excluir esta categoria pois existem produtos vinculados a ela.",
-        request.getRequestURI()
-    );
-    return ResponseEntity.status(status).body(err);
-  }
-
-  package vinix.resources.exceptions;
-
-import jakarta.servlet.http.HttpServletRequest;
-import org.slf me.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -48,13 +9,13 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import vinix.services.exceptions.EstoqueInsuficienteException;
-import vinix.services.exceptions.ProdutoExistente;
+
+import vinix.services.exceptions.ExistenteException;
 import vinix.services.exceptions.ResourceNotFoundException;
 
 import java.time.Instant;
 
-
+@Slf4j
 @ControllerAdvice
 public class ResourceExceptionHandler {
 
@@ -65,6 +26,7 @@ public class ResourceExceptionHandler {
       HttpServletRequest request) {
 
     HttpStatus status = HttpStatus.NOT_FOUND; //404
+
     StandardError err = new StandardError(
         Instant.now(),
         status.value(),
@@ -75,11 +37,13 @@ public class ResourceExceptionHandler {
     return ResponseEntity.status(status).body(err);
   }
 
-  // 400 - Produto/Regra de Negócio com Conflito ou Erro
-  @ExceptionHandler({ProdutoExistente.class,
-      IllegalArgumentException.class, EstoqueInsuficienteException.class})
-  public ResponseEntity<StandardError> businessError(RuntimeException e, HttpServletRequest request) {
-    HttpStatus status = HttpStatus.BAD_REQUEST;
+  @ExceptionHandler({ExistenteException.class})
+  public ResponseEntity<StandardError> businessError(
+      RuntimeException e,
+      HttpServletRequest request) {
+
+    HttpStatus status = HttpStatus.BAD_REQUEST; //400
+
     StandardError err = new StandardError(
         Instant.now(),
         status.value(),
@@ -90,10 +54,13 @@ public class ResourceExceptionHandler {
     return ResponseEntity.status(status).body(err);
   }
 
-  // 409 - Violação de Integridade do Banco (Ex: Excluir Categoria com Produtos vinculados)
   @ExceptionHandler(DataIntegrityViolationException.class)
-  public ResponseEntity<StandardError> dataIntegrity(DataIntegrityViolationException e, HttpServletRequest request) {
-    HttpStatus status = HttpStatus.CONFLICT;
+  public ResponseEntity<StandardError> dataIntegrity(
+      DataIntegrityViolationException e,
+      HttpServletRequest request) {
+
+    HttpStatus status = HttpStatus.CONFLICT;//409
+
     StandardError err = new StandardError(
         Instant.now(),
         status.value(),
@@ -104,10 +71,13 @@ public class ResourceExceptionHandler {
     return ResponseEntity.status(status).body(err);
   }
 
-  // 422 - Erros de Validação do Bean Validation (@Valid / @NotBlank / @Size)
   @ExceptionHandler(MethodArgumentNotValidException.class)
-  public ResponseEntity<ValidationError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+  public ResponseEntity<ValidationError> validation(
+      MethodArgumentNotValidException e,
+      HttpServletRequest request) {
+
     HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY; // 422
+
     ValidationError err = new ValidationError(
         Instant.now(),
         status.value(),
@@ -116,28 +86,28 @@ public class ResourceExceptionHandler {
         request.getRequestURI()
     );
 
-    // Captura todos os erros de campos no DTO e adiciona na lista 'errors'
     for (FieldError f : e.getBindingResult().getFieldErrors()) {
-      err.addError(f.getFieldName(), f.getDefaultMessage());
+      err.addError(f.getField(), f.getDefaultMessage());
     }
 
     return ResponseEntity.status(status).body(err);
   }
 
-  // 500 - Captura qualquer erro inesperado do Servidor
   @ExceptionHandler(Exception.class)
-  public ResponseEntity<StandardError> globalException(Exception e, HttpServletRequest request) {
-    log.error("Erro inesperado no servidor ao acessar {}: ", request.getRequestURI(), e);
+  public ResponseEntity<StandardError> globalException(
+      Exception e, HttpServletRequest request) {
+
+    log.error("Erro inesperado no servidor ao acessar {}: ",
+        request.getRequestURI(), e);
 
     HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR; // 500
     StandardError err = new StandardError(
         Instant.now(),
         status.value(),
         "Erro Interno no Servidor",
-        "Ocorreu um erro inesperado. Por favor, tente novamente mais tarde.",
+        "Ocorreu um erro inesperado, Por favor, tente novamente mais tarde",
         request.getRequestURI()
     );
     return ResponseEntity.status(status).body(err);
   }
-}
 }
