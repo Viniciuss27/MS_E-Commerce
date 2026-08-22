42c16c30aa5ad7f15cd7a739b1bbe60be90a7ac0
vinicius <viniciusdejjeuspessoal@gmail.com>
2026-08-22 11:29:34 -0300

    Notificação: criada

--- Config_Server/src/main/java/vinix/ConfigServerApplication.java
+++ Config_Server/src/main/java/vinix/ConfigServerApplication.java
@@ -1,7 +1,5 @@
 package vinix;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
